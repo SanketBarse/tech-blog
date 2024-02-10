efe09ca8535ba3271d0896c6fae79bf9ae5dbb5b
sanket <barsesanket007@gmail.com>
2024-02-10 23:08:41 +0530

    jakarta-added

--- src/main/java/helper/ConnectionProvider.java
+++ src/main/java/helper/ConnectionProvider.java
@@ -1,17 +1,27 @@
+
 package helper;
 
 
 import java.sql.*;
 
 public class ConnectionProvider {
+	
 	private static Connection con;
+	private static String url;
+	private static String username;
+	private static String password;
+	
 	
 	public static Connection getConnection() {
+		
+		url = "jdbc:mysql://localhost:3306/techblog";
+		username = "root";
+		password = "ROOT";
 		
 		try {		
 			if(con == null || con.isClosed()) {		
 				Class.forName("com.mysql.cj.jdbc.Driver");			
-				con = DriverManager.getConnection("jdbc:mysql://localhost:3306/techblog", "root", "ROOT");	
+				con = DriverManager.getConnection(url,username,password);	
 			}
 			
 		} catch (Exception e) {
